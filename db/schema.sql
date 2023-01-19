3d714c7f22864055b23153d528eda4e0e0ca0727
chelsbo <chelsea.reese@icloud.com>
2023-01-19 14:39:05 -0700

    Update

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,24 @@
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
+USE tracker_db;
+
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,2),
+  department_id INT
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,39 @@
--- Inserts names of departments into department table
-INSERT INTO department
-  (name)
+INSERT INTO department (name)
 VALUES
-  ('Engineering'),
-  ('Sales'),
-  ('Finance'),
-  ('Legal');
+    ('IT'),
+    ('Sales'),
+    ('Engineering'),
+    ('Customer Service'),
+    ('Data Science');
 
--- Inserts roles of employee into role table
-INSERT INTO role
-  (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
-  ('Software Engineer', 85000, 1),
-  ('Salesperson', 75000, 2),
-  ('Accountant', 125000, 3),
-  ('Lawyer', 200000, 4);
+    ('Cyber Secerity', 90000, 1),
+    ('IT Specialist', 70000, 1),
+    ('Customer Sales Solutions', 120000, 2),
+    ('Sales Rep', 70000, 2),
+    ('Data Engineer', 100000, 3),
+    ('Software Engineer', 120000, 3),
+    ('Call Center', 65000, 4),
+    ('Customer Service', 55000, 4),
+    ('Data Scientist', 100000, 5),
+    ('Data Engineer', 100000, 5);
 
--- Inserts employee information into employee table
-INSERT INTO employee
-  (first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-  ('Juan', 'Garcia', 1, 4),
-  ('Jonathan', 'Villcapoma', 2, 3),
-  ('Jesus', 'Meraz', 3, 1),
-  ('Estefany', 'Munoz', 4, 5);
+    ('Viserys', 'Targaryan', 2,2),
+    ('Robb', 'Stark', 5, 5),
+    ('Bran', 'Stark', 1, 1),
+    ('Ned', 'Stark', 1, 2),
+    ('Joffrey', 'Baratheon', 3,2),
+    ('Tommen', 'Baratheon', 1, 5),
+    ('Khal', 'Drogo', 4, 1),
+    ('Cersei', 'Lannister', 3, 2),
+    ('Jamie', 'Lanister', 2,5),
+    ('Jon', 'Snow', 5, 1),
+    ('Grey', 'Worm', 3, 1),
+    ('Sansa', 'Stark', 4, 4),
+    ('Davos', 'Seaworth', 4,null),
+    ('Margaery', 'Tyrell', 2, null),
+    ('Tormund', 'Giantsbane', 3, null),
+    ('Jorah', 'Mormont', 5, null)
